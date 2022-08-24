1a048aeb12dff830fabbbe72aba6061b7e07b422
vinicius <vinc-analist@hotmail.com>
2022-08-23 22:06:51 -0300

    commit inicial

---
+++ src/test/java/com/proj/estudos/meli/MeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.estudos.meli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
